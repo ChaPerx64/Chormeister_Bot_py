a03c4b0dd11cc71fb5039decffdd763fece1ccbb
@CheParx64 <chayan.par@gmail.com>
2023-04-18 21:04:41 +0200

    Deploy script divided to Deploy and Rub script

--- run.sh
+++ run.sh
@@ -2,12 +2,10 @@
 SRVR="root@188.225.86.226"
 EXECUTABLE="cmbot_main.py"
 TARDIR="/root/bots/chormeister_bot"
-ssh $SRVR "mkdir $TARDIR"
-scp *.py "requirements.txt" "$SRVR:$TARDIR"
 ssh $SRVR << EOF
+pkill -f "$EXECUTABLE"
 cd "$TARDIR"
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+nohup python3 "$EXECUTABLE" >/dev/null 2>&1 &
 EOF
-bash run.sh
